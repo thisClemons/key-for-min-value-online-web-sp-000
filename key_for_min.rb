4b8957dfd77a4c404fe2efd600b317b799872e1d
Adam Clemons <adamlouisclemons@gmail.com>
2021-02-20 15:57:03 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,10 @@
 
 def key_for_min_value(name_hash)
 
+  min_key, min_value = name_hash.first  
+
+  name_hash.each do |item, number|
+    min_key = item if number < min_value
+  end
+  min_key
 end
